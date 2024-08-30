0e71a71f6c26b38df6aa900a47d00773b2567431
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-08-30 14:03:47 +0100

    filter users

--- client/src/app/filled-files/filled-files.component.ts
+++ client/src/app/filled-files/filled-files.component.ts
@@ -3,26 +3,31 @@ import { MatTableDataSource } from '@angular/material/table';
 import { FilledFileService } from '../services/filled-file.service';
 import { ChangeDetectorRef } from '@angular/core';
 import { FilledFile } from '../File';
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-filled-files',
   templateUrl: './filled-files.component.html',
   styleUrls: ['./filled-files.component.css'],
+  providers: [DatePipe]
 })
 export class FilledFilesComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'filename','userType', 'uploadDate','actions'];
+  displayedColumns: string[] = ['id', 'filename', 'userType', 'uploadDate', 'actions'];
   dataSource = new MatTableDataSource<FilledFile>();
 
   constructor(
     private filledFileService: FilledFileService,
     private cdr: ChangeDetectorRef,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit(): void {
     this.filledFileService.getFilledFiles().subscribe((data: FilledFile[]) => {
-      this.dataSource.data = data;
-      console.log(data)
+    
+      this.dataSource.data = data.map(file => ({
+        ...file,
+        uploadDate: this.datePipe.transform(file.uploadDate, 'dd/MM/yyyy') || '' // Use empty string if null
+      }));
       this.cdr.detectChanges();
     });
   }
